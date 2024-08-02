0a92f2ab64b3eca7e128cfd0067fe345707aacc0
sinedis-soft <krasilou.a@dsd-insurance.by>
2024-08-02 11:28:54 +0300

    feat: add form RB-OSGO-FL

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,28 @@
-
-  (function ($) {
-  
+(function ($) {
   "use strict";
 
-    // PRE LOADER
-    $(window).load(function(){
-      $('.preloader').delay(500).slideUp('slow'); // set duration in brackets    
-    });
+  // PRE LOADER
+  $(window).load(function () {
+    $(".preloader").delay(500).slideUp("slow"); // set duration in brackets
+  });
 
-    // NAVBAR
-    $(".navbar").headroom();
+  // NAVBAR
+  $(".navbar").headroom();
 
-    $('.navbar-collapse a').click(function(){
-        $(".navbar-collapse").collapse('hide');
-    });
+  $(".navbar-collapse a").click(function () {
+    $(".navbar-collapse").collapse("hide");
+  });
 
-    $('.slick-slideshow').slick({
-      autoplay: true,
-      infinite: true,
-      arrows: false,
-      fade: true,
-      dots: false,
-    });
+  $(".slick-slideshow").slick({
+    autoplay: true,
+    infinite: true,
+    arrows: false,
+    fade: true,
+    dots: false,
+  });
 
-    $('.slick-testimonial').slick({
-      arrows: false,
-      dots: true,
-    });
-    
-  })(window.jQuery);
+  $(".slick-testimonial").slick({
+    arrows: false,
+    dots: true,
+  });
+})(window.jQuery);
